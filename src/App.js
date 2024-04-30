535d17d0d286d4a50b4455e255fa53e2b8084740
minji <lee.minji9521@gmail.com>
2024-04-29 20:22:39 -0400

    current longitude latitude

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+
+  useEffect(() => {
+    getLocation()
+  }, []) // when []is empty, it activates as soon as it the App renders. 
+
+  const x = document.getElementById("demo");
+
+  const getLocation = () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position)=>{
+      let latitude = position.coords.latitude
+      let longitude = position.coords.longitude
+
+    });
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        
     </div>
   );
 }
