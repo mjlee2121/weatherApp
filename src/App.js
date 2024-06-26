325eb342f55850176ebe577086d3c51957c07568
minji <lee.minji9521@gmail.com>
2024-06-26 13:09:49 -0400

    fixed ReactDOM.render is no longer supported in React18

--- src/App.js
+++ src/App.js
@@ -1,31 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
-import { useEffect } from 'react';
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { API_KEY } from './constants.js'
+import WeatherBox from './components/WeatherBox.js'
+import WeatherButton from './components/WeatherButton.js'
+import ClipLoader from "react-spinners/ClipLoader"
 
-function App() {
+const App = () => {
+  const [ weather, setWeather ]= useState(null)
+  const cities =['Paris','New york','Boston', 'Seoul']
+  const [city, setCity] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const getCurrentLocation = () => {
+  
+    navigator.geolocation.getCurrentPosition((position)=>{
+      let lat = position.coords.latitude
+      let lon = position.coords.longitude
+
+      getWeatherByCurrentLocation(lat, lon)
+
+    })
+  }
+
+  const getWeatherByCurrentLocation = async(lat, lon) => {
+      let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+      let response = await fetch(url)
+      let data = await response.json()
+
+      console.log('data is :', data)
+  }
+  
+  const getWeatherByCity = async(city) => {
+    console.log('getting weather by city')
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+    setLoading(true)
+    let response = await fetch(url)
+    let data = await response.json()
+    console.log('data by city', data)
+    setLoading(false)
+    setWeather(data)
+
+  }
 
   useEffect(() => {
-    getLocation()
-  }, []) // when []is empty, it activates as soon as it the App renders. 
+    if (city===''){
+      console.log('mj no city')
+      getCurrentLocation()
+      
+    }
+    else{
+      getWeatherByCity(city)
+    }
+  }, [city]) // when []is empty, it activates as soon as it the App renders. 
 
-  const x = document.getElementById("demo");
-
-  const getLocation = () => {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition((position)=>{
-      let latitude = position.coords.latitude
-      let longitude = position.coords.longitude
-
-    });
-  } else {
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
 
   return (
-    <div className="App">
+    <div>
+        {loading ? (
+        <div className='container'>
+          <ClipLoader color='blue' loading={loading} size={150}></ClipLoader> 
+        </div>) :
+        (
+        <div className='container'>
+          <WeatherBox weather={weather}/>
+          <WeatherButton cities={cities} setCity={setCity}/>
+          
+        </div>
+        )}
         
+        
+      <div>
+
+      </div>
     </div>
+
+    
   );
 }
 
