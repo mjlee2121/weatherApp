8c1cac31d554bcaabfd32458a9177c83de3d4b5a
minji <lee.minji9521@gmail.com>
2024-06-26 15:20:08 -0400

    added try catch

--- src/App.js
+++ src/App.js
@@ -7,12 +7,29 @@ import WeatherButton from './component/WeatherButton.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import ClipLoader from "react-spinners/ClipLoader"
 
+const cities =['Paris','New york','Boston', 'Seoul']
+
 const App = () => {
 
-  const [ weather, setWeather ]= useState(null)
-  const cities =['Paris','New york','Boston', 'Seoul']
+  const [weather, setWeather]= useState(null)
   const [city, setCity] = useState('')
   const [loading, setLoading] = useState(false)
+  const [apiError, setAPIError] = useState("")
+  
+  const getWeatherByCurrentLocation = async(lat, lon) => {
+    try{
+      let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+      let response = await fetch(url)
+      let data = await response.json()
+  
+      setWeather(data)
+      setLoading(false)
+    } catch (err){
+      setAPIError(err.message)
+      setLoading(false)
+    }
+    
+}
 
   const getCurrentLocation = () => {
   
@@ -25,20 +42,13 @@ const App = () => {
     })
   }
   const handleCityChange = (city) => {
-    if (city == "current"){
+    if (city === "current"){
       setCity(null)
     }else{
       setCity(city)
     }
   }
-  const getWeatherByCurrentLocation = async(lat, lon) => {
-      let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
-      let response = await fetch(url)
-      let data = await response.json()
-
-      setWeather(data)
-      setLoading(false)
-  }
+  
   
   const getWeatherByCity = async() => {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
@@ -67,13 +77,14 @@ const App = () => {
         {loading ? (
         <div className='container'>
           <ClipLoader color='blue' loading={loading} size={150}></ClipLoader> 
-        </div>) :
-        (
-        <div className='container'>
-          <WeatherBox weather={weather}/>
-          <WeatherButton cities={cities} selectedCity={city} handleCityChange={handleCityChange}/>
+        </div>) : !apiError ? (
+          <div className='container'>
+            <WeatherBox weather={weather}/>
+            <WeatherButton cities={cities} selectedCity={city} handleCityChange={handleCityChange}/>
           
-        </div>
+          </div>
+        ) : (
+          apiError
         )}
         
         
--- src/component/WeatherButton.js
+++ src/component/WeatherButton.js
@@ -5,13 +5,13 @@ const WeatherButton = ({cities, selectedCity, handleCityChange}) => {
 
   return (
     <div>
-        <div>
-          <Button variant={`${selectedCity == null ? "outline-warning" : "warning"}`} onClick={()=>handleCityChange("current")}>
+        <div className="menu-container">
+          <Button variant={`${selectedCity === null ? "outline-warning" : "warning"}`} onClick={()=>handleCityChange("current")}>
             Current Location
           </Button>
 
           {cities.map((city)=>
-            (<Button variant={`${selectedCity == city ? "outline-warning" : "warning"}`} onClick={()=>handleCityChange(city)}>
+            (<Button variant={`${selectedCity === city ? "outline-warning" : "warning"}`} onClick={()=>handleCityChange(city)}>
               {city} 
             </Button>)
           )}
