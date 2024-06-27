9624b54b633ab3587e4623e1f711acbd28440db7
minji <lee.minji9521@gmail.com>
2024-06-27 15:10:17 -0400

    sidenav placed

--- src/component/Sidebar.js
+++ src/component/Sidebar.js
@@ -8,9 +8,9 @@ function Sidebar() {
         <ui className='SidebarList'>
         {SidebarData.map((val, key) =>{
             return (
-                <li className='row' key={key} onClick={()=> {window.location.pathname = val.link}}>
-                    <div>{val.icon}</div> 
-                    <div>{val.title}</div>
+                <li className='row' id={window.location.pathname == val.link ? 'active':''} key={key} onClick={()=> {window.location.pathname = val.link}}>
+                    <div id='icon'>{val.icon}</div> 
+                    <div id='title'>{val.title}</div>
                 </li>
             )
         })}
